3b84d75712a3945217351f95e620aa6df4d31e75
seiroise <seiroise@gmail.com>
2016-10-02 15:06:28 +0900

    PolyLine2DAdjusterにRemoveModeを追加

---
+++ Assets/Seiro/Scripts/Geometric/Polygon/Concave/ConcavePolygonEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Seiro.Scripts.Graphics.PolyLine2D;
+
+namespace Seiro.Scripts.Geometric.Polygon.Concave {
+
+	/// <summary>
+	/// 凹多角形の編集
+	/// </summary>
+	public class ConcavePolygonEditor : MonoBehaviour {
+
+		[Header("PolyLineEditor")]
+		public PolyLine2DEditor lineEditor;
+
+	}
+}
---
+++ Assets/Seiro/Scripts/Graphics/PolyLine2D/Snap/BaseSnap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Seiro.Scripts.Graphics.PolyLine2D.Snap {
+
+	/// <summary>
+	/// ある座標に対してスナップ処理を行う基底クラス
+	/// </summary>
+	public abstract class BaseSnap {
+
+		protected float snapForce;
+
+		public BaseSnap(float snapForce) {
+			this.snapForce = snapForce;
+		}
+
+		public abstract bool Snap(Vector2 input, out Vector2 output);
+
+		public abstract EasyMesh GetEasyMesh(Color color);
+	}
+}
